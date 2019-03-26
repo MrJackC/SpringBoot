abdef4675a9b855b9af0e0939d63dabb3f9e0e48
MrJackC <845886914@qq.com>
2019-03-26 22:17:38 +0800

    SpringBoot  处理异常   自定义异常界面

---
+++ 06-Spring Boot异常处理/01-SpringBoot异常处理-自定义错误页面/14-spring-boot-exception1/src/main/java/com/mrjason/controller/DemoController.java
@@ -0,0 +1,29 @@
+package com.mrjason.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: MrJason
+ * Date: 2019-03-26
+ * Time: 21:59
+ * SpringBoot  处理方式一   自定义错误页面
+ */
+@Controller
+public class DemoController {
+    @RequestMapping("/show")
+    public String  showInfo(){
+        String str = null;
+        str.length();
+        return "index";
+    }
+
+    @RequestMapping("/show2")
+    public String  showInfo2(){
+        int a = 100/0;
+        return "index";
+    }
+
+}
