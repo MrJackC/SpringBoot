716138842fec6a505932bac74d19de20eccb896c
mrjason <mrjasonss@163.com>
2019-03-29 11:54:16 +0800

    SpringBoot 整合Ehcache  中Cacheable 的key属性的使用

---
+++ 09-Spring Boot缓存技术/02-Spring Boot整合@Cacheable注解使用/23-spring-boot-ehcache/src/main/java/com/mrjason/service/impl/UsersServiceImpl.java
@@ -0,0 +1,44 @@
+package com.mrjason.service.impl;
+
+import com.mrjason.dao.UsersRepository;
+import com.mrjason.pojo.Users;
+import com.mrjason.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+
+public class UsersServiceImpl implements UsersService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+    @Override
+    public List<Users> findUsersAll() {
+        return usersRepository.findAll();
+    }
+
+    @Override
+
+    @Cacheable (value = "users")  //指定ehcache文件中的缓存策略
+    public Users findUserById(Integer id) {
+        return this.usersRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    // 对分页数据进行缓存处理
+    @Cacheable(value = "users" ,key = "#pageable.pageSize")
+    public Page<Users> findUserByPage(Pageable pageable) {
+        return this.usersRepository.findAll(pageable);
+    }
+
+    @Override
+    public void saveUsers(Users users) {
+        this.usersRepository.save(users);
+    }
+}
---
+++ 09-Spring Boot缓存技术/02-Spring Boot整合@Cacheable注解使用/23-spring-boot-ehcache/src/test/com/mrjason/test/UsersServiceTest.java
@@ -0,0 +1,38 @@
+package com.mrjason.test;
+
+import com.mrjason.Application;
+import com.mrjason.service.UsersService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class UsersServiceTest {
+    @Autowired
+    private UsersService usersService;
+    @Test
+    public void testFindUsersById(){
+        System.out.println(this.usersService.findUserById(1));
+        System.out.println(this.usersService.findUserById(1));
+    }
+
+    @Test
+    public  void testFindUserByPage(){
+        Pageable pageable = new PageRequest(0,2);
+        //第一次查询
+        System.out.println(this.usersService.findUserByPage(pageable).getTotalElements());
+        //第二次查询
+        System.out.println(this.usersService.findUserByPage(pageable).getTotalElements());
+        //第三次查询
+        pageable = new PageRequest(1,2);
+        System.out.println(this.usersService.findUserByPage(pageable).getTotalElements());
+
+    }
+}
