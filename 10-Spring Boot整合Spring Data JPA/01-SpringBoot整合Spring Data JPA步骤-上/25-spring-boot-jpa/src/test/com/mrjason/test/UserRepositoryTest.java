36ca35c040b68ccd0760b7341ffc1ea245ad8d2f
mrjason <mrjasonss@163.com>
2019-03-30 16:51:21 +0800

    Springboot JPA Query 用法

--- 10-Spring Boot整合Spring Data JPA/01-SpringBoot整合Spring Data JPA步骤-上/25-spring-boot-jpa/src/test/com/mrjason/test/UserRepositoryTest.java
+++ 10-Spring Boot整合Spring Data JPA/01-SpringBoot整合Spring Data JPA步骤-上/25-spring-boot-jpa/src/test/com/mrjason/test/UserRepositoryTest.java
@@ -3,12 +3,15 @@ package com.mrjason.test;
 import com.mrjason.Application;
 import com.mrjason.dao.UserRepositoryByName;
 import com.mrjason.dao.UsersRepository;
+import com.mrjason.dao.UsersRepositoryQueryAnnotation;
 import com.mrjason.pojo.Users;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,6 +24,10 @@ public class UserRepositoryTest {
 
     @Autowired
     private UserRepositoryByName userRepositoryByName;
+
+
+    @Autowired
+    private UsersRepositoryQueryAnnotation usersRepositoryQueryAnnotation;
     @Test
     public void testSava(){
         Users users = new Users();
@@ -45,5 +52,21 @@ public class UserRepositoryTest {
         for(Users users : list){
             System.out.println(users);
         }
+    }
+
+
+    @Test
+    public void queryByNameUseHQL(){
+        List<Users> list = this.usersRepositoryQueryAnnotation.queryByNameUseHQL("hhh");
+        for(Users users : list){
+            System.out.println(users);
+        }
+    }
+
+    @Test
+    @Transactional//Transactional与Test  一起使用  事务自动回滚
+    @Rollback  //取消回滚
+    public void updateUserNameById(){
+        this.usersRepositoryQueryAnnotation.updateUsersNameById("666",1);
     }
 }
